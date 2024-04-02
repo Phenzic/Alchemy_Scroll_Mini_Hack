bae9e4f8dcb040d6ea6b4c4ed5cbe4470e1df0de
kinyichukwu <kinyichukwuose@gmail.com>
2024-04-02 11:14:49 +0100

    Auth Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
 import "./App.css";
+import { Route, Routes } from "react-router";
+import { Login } from "./pages/auth/Login";
+import { Register } from "./pages/auth/Register";
+import { ForgotPassword } from "./pages/auth/ForgotPassword";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/auth/ForgotPassword.jsx
@@ -0,0 +1,64 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Input } from "../../components/input";
+import AuthButton from "../../components/auth_button";
+import AuthHeading from "../../components/auth_heading";
+import { getName } from "../../utils/helper";
+
+const defaultValue = {
+  email_address: "",
+};
+
+export const ForgotPassword = () => {
+  const [state, setstate] = useState("unloaded");
+  const [formField, setFormField] = useState(defaultValue);
+
+  const resetFormFields = () => {
+    setFormField(defaultValue);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <AuthHeading />
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            {Object.keys(formField).map((key) => {
+              return (
+                <Input
+                  key={key}
+                  name={getName(key)}
+                  type={"text"}
+                  value={formField[key]}
+                  placeholder={getName(key)}
+                  onChange={(e) => {
+                    setFormField((prev) => ({
+                      ...prev,
+                      [key]: e.target.value,
+                    }));
+                  }}
+                />
+              );
+            })}
+
+            <AuthButton />
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not registered?{" "}
+            <a
+              href="#"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Sign Up
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
