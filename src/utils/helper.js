937447236f78bf620b4e5eaf258a15bd20e7375b
kinyichukwu <kinyichukwuose@gmail.com>
2024-04-05 17:47:52 +0100

    Init

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -3,3 +3,15 @@ export const getName = (str) =>
     .split("_")
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
     .join(" ");
+
+export const register = async () => {
+  const res = await axios.post("http://localhost:3000/auth/register", {
+    name: "John Doe",
+    email_address: "",
+    password: "",
+  });
+
+  console.log(res.data);
+};
+
+
